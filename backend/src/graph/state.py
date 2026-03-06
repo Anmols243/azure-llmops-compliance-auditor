a99e557b5e95cb9b0c7e83f2255d013b2bba24ab
Anmol Singh <168528590+Anmols243@users.noreply.github.com>
2026-03-07 03:51:57 +0530

    Added Basic Structure

---
+++ backend/src/graph/state.py
@@ -0,0 +1,27 @@
+import operator
+from typing import Annotated, List, Dict, Optional, Any, TypedDict
+
+class ComplianceIssue(TypedDict):
+    category : str
+    description : str
+    severity : str
+    timestamp : Optional[str] 
+
+class VideoAuditState(TypedDict):
+    '''
+    Defines the data schema for langgraph execution content 
+    '''
+    vid_url : str
+    vid_id : str
+
+    local_path : Optional[str]
+    video_metadata : Dict[str,Any]
+    transcript : Optional[str]
+    ocr_test : List[str]
+
+    compliance_results : Annotated[List[ComplianceIssue], operator.add]
+
+    final_status : str
+    final_report : str
+
+    errors : Annotated[List[str], operator.add]
